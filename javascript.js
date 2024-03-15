3c96b2b5a20af274e8cd23d9c2c30827386850a4
Zowell <67911888+zowell29@users.noreply.github.com>
2024-03-15 17:26:57 +0200

    Add the posibility for the player to press buttons Add the results on the UI Modify the HTML content

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ function getComputerChoice() {
     let computerChoide = choices[(Math.floor(Math.random() * choices.length))];
     //will return rock, paper or scissors
     //will be used to make computer play
-    console.log(computerChoide);
+    computerChoideDiv.textContent =`Computer Choice: ${computerChoide}`;
     return computerChoide;
 }
 
@@ -11,32 +11,50 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
         playerScore = playerScore + 1;
         computerScore = computerScore + 1;
-        return "Draw. Both of you are strong enough!";
+        resultLine.textContent = `Draw. Both of you are strong enough!  Player Score: ${playerScore}` + "\nComputerScore: " + `${computerScore}`;
+        // return "Draw. Both of you are strong enough!";
     };
     if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors" ) {
         playerScore = playerScore + 1;
-        return "Player won. Rock is better than Scissors!";
+        resultLine.textContent = `Player won. Rock is better than Scissors! Player Score: ${playerScore}    ComputerScore: ${computerScore}`;
+        // return "Player won. Rock is better than Scissors!";
     };
     if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper" ){
         computerScore = computerScore + 1;
-        return "Computer won. Paper is better than Rock!";
+        resultLine.textContent = `Computer won. Paper is better than Rock!  Player Score: ${playerScore}    ComputerScore: ${computerScore}`;
+        // return "Computer won. Paper is better than Rock!";
     };
     if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock" ){
         playerScore = playerScore + 1;
-        return "Player won. Paper is better than Rock!";
+        resultLine.textContent = `Player won. Paper is better than Rock!  Player Score: ${playerScore}    ComputerScore: ${computerScore}`;
+        // return "Player won. Paper is better than Rock!";
     };
     if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors" ){
         computerScore = computerScore + 1;
-        return "Computer won. Scissors are better than Paper!";
+        resultLine.textContent = `Computer won. Scissors are better than Paper!  Player Score: ${playerScore}    ComputerScore: ${computerScore}`;
+        // return "Computer won. Scissors are better than Paper!";
     };
     if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock" ){
         computerScore = computerScore + 1;
-        return "Computer won. Rock is better than Scissors!";
+        resultLine.textContent = `Computer won. Rock is better than Scissors!  Player Score: ${playerScore}    ComputerScore: ${computerScore}`;
+        // return "Computer won. Rock is better than Scissors!";
     };
     if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper" ){
         playerScore = playerScore + 1;
-        return "Player won. Scissors are better than Paper!";
+        resultLine.textContent = `Player won. Scissors are better than Paper!  Player Score: ${playerScore}    ComputerScore: ${computerScore}`;
+        // return "Player won. Scissors are better than Paper!";
     };
+    if (playerScore == 5) {
+        resultLine.textContent = `Player won the game! Player Score: ${playerScore} ComputerScore: ${computerScore}`;
+        playerScore = 0;
+        computerScore = 0;
+    }
+    if (computerScore == 5) {
+        resultLine.textContent = `Computer won the game! Player Score: ${playerScore} ComputerScore: ${computerScore}`;
+        playerScore = 0;
+        computerScore = 0;
+    }
+
 };
 
 function playGame(roundsNumber){
@@ -63,6 +81,42 @@ function playGame(roundsNumber){
 }
 var computerScore = 0;
 var playerScore = 0;
-const rounds = 5;
+// const rounds = 5;
 
-playGame(rounds);
+// playGame(rounds);
+
+// const rockBtn = document.querySelector("#rock");
+// const paperBtn = document.querySelector("#paper");
+// const scissorsBtn = document.querySelector("#scissors");
+const resultsDiv = document.querySelector('#results');
+const buttons = document.querySelectorAll('button');
+
+
+// rockBtn.addEventListener('click', ()=> {
+//     resultLine.textContent = `PlayerChoice: Rock`;
+//     playRound("rock", getComputerChoice());
+// })
+// paperBtn.addEventListener('click', ()=> {
+//     resultLine.textContent = `PlayerChoice: Paper`;
+//     playRound("paper", getComputerChoice());
+// })
+// scissorsBtn.addEventListener('click', ()=> {
+//     resultLine.textContent = `PlayerChoice: Scissors`;
+//     playRound("scissors", getComputerChoice());
+// })
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playerChoiceDiv.textContent = `Player Choice: ${button.value}`;
+        playRound(button.value, getComputerChoice());
+    })
+})
+
+const resultLine = document.createElement('div');
+const computerChoideDiv = document.createElement('div');
+const playerChoiceDiv = document.createElement('div');
+resultsDiv.appendChild(playerChoiceDiv);
+resultsDiv.appendChild(computerChoideDiv);
+resultLine.textContent = ""
+resultsDiv.appendChild(resultLine);
+
